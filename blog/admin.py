5b672040c6e5ce2b649727099c0af21dc0f2204a
Digvijay Bhakuni <digvijay.bhakuni@rediffmail.com>
2016-05-27 00:24:36 +0530

    Changes to customize the django admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,41 @@ from django.contrib import admin
 from .models import Post, Author
 # Register your models here.
 
-admin.site.register(Post)
+
+class AuthorInline(admin.StackedInline):
+    """
+    This make Author to be create in any form as
+    part of it self not as separate form
+    it was like open new pop-up with using this
+    """
+    model = Author
+
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    This PostAdmin class extends the admin.ModelAdmin
+    to modifiy and customise the look and feel of
+    the Admin Panel Form for the Post Model
+    """
+
+    # fields = ['title', 'author']
+    # To show only fields in the fields list this code uncomment will not work as other fields are also required
+
+    """
+    This make from in categories manger
+    """
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Post Content', {'fields': ['content']}),
+        ('Meta Info', {'fields': ['author', 'datePub'], 'classes': ['collapse']}),
+    ]
+
+    """
+    This help to add more columns in this list view of post
+    """
+    list_display = ('title', 'datePub', 'author', 'was_published_recently')
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
+
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 class Author(models.Model):
@@ -19,6 +21,12 @@ class Post(models.Model):
     def __str__(self):
         return " Post Title : " + self.title
 
+    def was_published_recently(self):
+        return self.datePub >= (timezone.now() - datetime.timedelta(days=10)).date()
+
+    was_published_recently.admin_order_field = 'datePub'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 """
     Using database models
